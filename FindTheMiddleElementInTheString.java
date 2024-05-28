217b453f74ee09d23c0fa8a86e523d860d3d827c
Y Venkata Ravi <156241322+VenkataRavi2003@users.noreply.github.com>
2024-05-28 18:51:09 +0530

    Add files via upload

---
+++ FindTheMiddleElementInTheString.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class FindTheMiddleElementInTheString{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("\nEnter the string -> ");
+		String str=sc.nextLine();
+		int middleElementIndex=str.length()/2;
+		System.out.println("\nMiddle Element In The -> "+str+" is : "+str.charAt(middleElementIndex));
+	}
+}
